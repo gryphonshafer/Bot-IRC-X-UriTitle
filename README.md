4c0d14d423300554c51858da423e97bb0dabbc17
Gryphon Shafer <gryphon@goldenguru.com>
2016-11-09 11:19:10 -0800

    Initial commit for v1.01

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# Bot-IRC-X-UriTitle
-Bot::IRC plugin to parse and print URI titles
+# NAME
+
+Bot::IRC::X::UriTitle - Bot::IRC plugin to parse and print URI titles
+
+# VERSION
+
+version 1.01
+
+[![Build Status](https://travis-ci.org/gryphonshafer/Bot-IRC-X-UriTitle.svg)](https://travis-ci.org/gryphonshafer/Bot-IRC-X-UriTitle)
+[![Coverage Status](https://coveralls.io/repos/gryphonshafer/Bot-IRC-X-UriTitle/badge.png)](https://coveralls.io/r/gryphonshafer/Bot-IRC-X-UriTitle)
+
+# SYNOPSIS
+
+    use Bot::IRC;
+
+    Bot::IRC->new(
+        connect => { server => 'irc.perl.org' },
+        plugins => ['UriTitle'],
+    )->run;
+
+# DESCRIPTION
+
+This [Bot::IRC](https://metacpan.org/pod/Bot::IRC) plugin makes the bot parse and print URI titles.
+
+# SEE ALSO
+
+You can look for additional information at:
+
+- [Bot::IRC](https://metacpan.org/pod/Bot::IRC)
+- [GitHub](https://github.com/gryphonshafer/Bot-IRC-X-UriTitle)
+- [CPAN](http://search.cpan.org/dist/Bot-IRC-X-UriTitle)
+- [MetaCPAN](https://metacpan.org/pod/Bot::IRC::X::UriTitle)
+- [AnnoCPAN](http://annocpan.org/dist/Bot-IRC-X-UriTitle)
+- [Travis CI](https://travis-ci.org/gryphonshafer/Bot-IRC-X-UriTitle)
+- [Coveralls](https://coveralls.io/r/gryphonshafer/Bot-IRC-X-UriTitle)
+- [CPANTS](http://cpants.cpanauthors.org/dist/Bot-IRC-X-UriTitle)
+- [CPAN Testers](http://www.cpantesters.org/distro/T/Bot-IRC-X-UriTitle.html)
+
+# AUTHOR
+
+Gryphon Shafer <gryphon@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2016 by Gryphon Shafer.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
---
+++ lib/Bot/IRC/X/UriTitle.pm
@@ -0,0 +1,74 @@
+package Bot::IRC::X::UriTitle;
+# ABSTRACT: Bot::IRC plugin to parse and print URI titles
+
+use strict;
+use warnings;
+
+use Text::Unidecode 'unidecode';
+use URI::Title 'title';
+
+# VERSION
+
+sub init {
+    my ($bot) = @_;
+
+    $bot->hook(
+        {
+            command => 'PRIVMSG',
+            text    => qr|https?://\S+|,
+        },
+        sub {
+            my ( $bot, $in, $m ) = @_;
+
+            my %urls;
+            $urls{$1} = 1 while ( $in->{text} =~ m|(https?://\S+)|g );
+            $bot->reply( '[ ' . unidecode( title($_) ) . ' ]' ) for ( keys %urls );
+        },
+    );
+}
+
+1;
+__END__
+=pod
+
+=begin :badges
+
+=for markdown
+[![Build Status](https://travis-ci.org/gryphonshafer/Bot-IRC-X-UriTitle.svg)](https://travis-ci.org/gryphonshafer/Bot-IRC-X-UriTitle)
+[![Coverage Status](https://coveralls.io/repos/gryphonshafer/Bot-IRC-X-UriTitle/badge.png)](https://coveralls.io/r/gryphonshafer/Bot-IRC-X-UriTitle)
+
+=end :badges
+
+=for test_synopsis BEGIN { die "SKIP: Bot::IRC may or may not be available." }
+
+=head1 SYNOPSIS
+
+    use Bot::IRC;
+
+    Bot::IRC->new(
+        connect => { server => 'irc.perl.org' },
+        plugins => ['UriTitle'],
+    )->run;
+
+=head1 DESCRIPTION
+
+This L<Bot::IRC> plugin makes the bot parse and print URI titles.
+
+=head1 SEE ALSO
+
+You can look for additional information at:
+
+=for :list
+* L<Bot::IRC>
+* L<GitHub|https://github.com/gryphonshafer/Bot-IRC-X-UriTitle>
+* L<CPAN|http://search.cpan.org/dist/Bot-IRC-X-UriTitle>
+* L<MetaCPAN|https://metacpan.org/pod/Bot::IRC::X::UriTitle>
+* L<AnnoCPAN|http://annocpan.org/dist/Bot-IRC-X-UriTitle>
+* L<Travis CI|https://travis-ci.org/gryphonshafer/Bot-IRC-X-UriTitle>
+* L<Coveralls|https://coveralls.io/r/gryphonshafer/Bot-IRC-X-UriTitle>
+* L<CPANTS|http://cpants.cpanauthors.org/dist/Bot-IRC-X-UriTitle>
+* L<CPAN Testers|http://www.cpantesters.org/distro/T/Bot-IRC-X-UriTitle.html>
+
+=for Pod::Coverage init
+
+=cut
